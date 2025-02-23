673f9678f468846536e3789b5b62d2cc282519d3
Motoshi Suzuki <101000230+motoshi-suzuki-hp@users.noreply.github.com>
2025-02-24 00:28:53 +0900

    母語の追加

--- db/users.sql
+++ db/users.sql
@@ -4,6 +4,9 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     hashed_password VARCHAR(200) NOT NULL,
+    first_language VARCHAR(50),
+    second_language VARCHAR(50),
+    third_language VARCHAR(50),
     role ENUM('user', 'admin') DEFAULT 'user',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
